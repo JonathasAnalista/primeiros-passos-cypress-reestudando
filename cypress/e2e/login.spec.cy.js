324695b5bb5b39290e56723eba70e04f28b2ee2e
jonat@Jonas-PC <jonathasguilherme40@gmail.com>
2025-04-14 18:43:41 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,17 +1,27 @@
 describe('Orange HRM Tests', () => {
+
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "[role='alert']"
+
+  }
+
   it('Login - Success', () => { //Se usar o .skip depois do it exemplo: it.skip (desliga esse test ele não irá executar)
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //verificando se a página está correta depois de logar. opção 1
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard') //verificando se a página está correta depois de logar. opção 2 
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard') //verificando se a página está correta depois de logar. opção 2 
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('teste')
-    cy.get("[name='password']").type('teste')
-    cy.get("[type='submit']").click()
-    cy.get("[role='alert']")
+    cy.get(selectorList.usernameField).type('teste')
+    cy.get(selectorList.passwordField).type('teste')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert)
   })
 })
